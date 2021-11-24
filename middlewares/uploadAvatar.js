180118ccab4e7507846d3ac20eb49341ba7236cb
AntonTymchenko <timchenkov_@ukr.net>
2021-11-24 14:20:38 +0200

    hw05
    добавил mimeType

--- middlewares/uploadAvatar.js
+++ middlewares/uploadAvatar.js
@@ -1,6 +1,8 @@
 const multer = require('multer')
 const path = require('path')
 
+const MIME_TYPE_LIST = require('../utils/mimeTypesList')
+
 const tempDir = path.join(__dirname, '../', 'temp')
 
 const uploadConfig = multer.diskStorage({
@@ -15,8 +17,13 @@ const uploadConfig = multer.diskStorage({
   },
 })
 
+const mimeTypeFilter = (req, file, cb) => {
+  MIME_TYPE_LIST[file.mimetype] ? cb(null, true) : cb(null, false)
+}
+
 const uploadAvatar = multer({
   storage: uploadConfig,
+  mimeTypeFilter,
 })
 
 module.exports = uploadAvatar
---
+++ utils/mimeTypesList.js
@@ -0,0 +1,8 @@
+const MIME_TYPE_LIST = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg',
+  'image/webp': 'webp',
+}
+
+module.exports = MIME_TYPE_LIST
